1741386b595e1a46ab5adf71525f7ebb09c9cb3b
Pierre Beaujean <pierreb24@gmail.com>
2020-01-27 16:48:24 +0100

    go for a new version

--- goto_publication/__init__.py
+++ goto_publication/__init__.py
@@ -3,7 +3,7 @@ goto-publication: citation-based DOI searches and redirections
 """
 
 __program_name__ = 'goto-publication'
-__version__ = '0.1'
+__version__ = '0.2'
 __author__ = 'Pierre Beaujean'
 __maintainer__ = 'Pierre Beaujean'
 __email__ = 'pierre.beaujean@unamur.be'
